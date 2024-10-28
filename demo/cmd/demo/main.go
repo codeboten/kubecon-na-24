70f6baf23cbe0156319b6cfc3b9c0ad48f25bad3
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-10-28 15:31:57 -0700

    initial commit
    This includes:
    - the go demo app with both config parsing and programmatic configuration
    - includes the start of a config file to use for demo
    
    NOTE: this relies on the env parsing currently in a branch in go PR
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

---
+++ demo/cmd/demo/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"go.opentelemetry.io/contrib/config"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/log/global"
+	"go.opentelemetry.io/otel/propagation"
+	otellog "go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/trace"
+
+	"github.com/codeboten/kubecon-na-24/demo/internal/server"
+)
+
+func main() {
+	ctx := context.Background()
+	// Set up OpenTelemetry.
+	otelShutdown, err := setupOTel(ctx)
+	if err != nil {
+		log.Fatalf("err: %v\n", err)
+	}
+	// Handle shutdown properly so nothing leaks.
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+		log.Fatalf("err: %v\n", err)
+	}()
+
+	server.Run()
+}
+
+func setupOTel(ctx context.Context) (func(context.Context) error, error) {
+	// load config file
+	confFlag := flag.String("config", "config.yaml", "config file to parse")
+	flag.Parse()
+	b, err := os.ReadFile(*confFlag)
+	if err != nil {
+		return nil, err
+	}
+	// parse the config
+	conf, err := config.ParseYAML(b)
+	if err != nil {
+		return nil, err
+	}
+	sdk, err := config.NewSDK(config.WithContext(ctx), config.WithOpenTelemetryConfiguration(*conf))
+	if err != nil {
+		return nil, err
+	}
+	otel.SetTracerProvider(sdk.TracerProvider())
+	otel.SetMeterProvider(sdk.MeterProvider())
+	global.SetLoggerProvider(sdk.LoggerProvider())
+	return sdk.Shutdown, nil
+}
+
+// setupOTelSDK bootstraps the OpenTelemetry pipeline.
+// If it does not return an error, make sure to call shutdown for proper cleanup.
+func setupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	// Set up propagator.
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	))
+
+	// Set up trace provider.
+	traceExporter, err := stdouttrace.New(
+		stdouttrace.WithPrettyPrint())
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	tracerProvider := trace.NewTracerProvider(
+		trace.WithBatcher(traceExporter,
+			// Default is 5s. Set to 1s for demonstrative purposes.
+			trace.WithBatchTimeout(time.Second)),
+	)
+
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Set up meter provider.
+	metricExporter, err := stdoutmetric.New()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithReader(metric.NewPeriodicReader(metricExporter,
+			// Default is 1m. Set to 3s for demonstrative purposes.
+			metric.WithInterval(3*time.Second))),
+	)
+	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	otel.SetMeterProvider(meterProvider)
+
+	// Set up logger provider.
+	logExporter, err := stdoutlog.New()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	loggerProvider := otellog.NewLoggerProvider(
+		otellog.WithProcessor(otellog.NewBatchProcessor(logExporter)),
+	)
+	shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
+	global.SetLoggerProvider(loggerProvider)
+
+	return
+}
