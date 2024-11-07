c4d882f7f87f208483a9098f1e2c916a64a985b4
Jack Berg <jberg@newrelic.com>
2024-11-07 16:55:41 -0600

    Use TUI in java example

--- demo-java/load-generator.sh
+++ demo-java/load-generator.sh
@@ -10,5 +10,14 @@ do
   curl -H "Accept: text/html" "http://localhost:8080/vets.html" > /dev/null || true
   echo
 
+    echo "GET http://localhost:8081/actuator/health"
+    curl "http://localhost:8081/actuator/health" || true
+    echo
+
+    echo "GET http://localhost:8081/vets.html"
+    curl -H "Accept: text/html" "http://localhost:8081/vets.html" > /dev/null || true
+    echo
+
+
   sleep 2
 done
