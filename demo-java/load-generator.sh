d383d8fc6fa4289511cc0c75d7ef88558215d081
Jack Berg <jberg@newrelic.com>
2024-11-12 08:03:17 -0600

    final tweaks

--- demo-java/load-generator.sh
+++ demo-java/load-generator.sh
@@ -6,16 +6,16 @@ do
   curl "http://localhost:8080/actuator/health" || true
   echo
 
-  echo "GET http://localhost:8080/vets.html"
-  curl -H "Accept: text/html" "http://localhost:8080/vets.html" > /dev/null || true
+  echo "GET http://localhost:8080/owners?lastName=l"
+  curl -H "Accept: text/html" "http://localhost:8080/owners?lastName=" > /dev/null || true
   echo
 
     echo "GET http://localhost:8081/actuator/health"
     curl "http://localhost:8081/actuator/health" || true
     echo
 
-    echo "GET http://localhost:8081/vets.html"
-    curl -H "Accept: text/html" "http://localhost:8081/vets.html" > /dev/null || true
+    echo "GET http://localhost:8081/owners?lastName="
+    curl -H "Accept: text/html" "http://localhost:8081/owners?lastName=" > /dev/null || true
     echo
 
 
